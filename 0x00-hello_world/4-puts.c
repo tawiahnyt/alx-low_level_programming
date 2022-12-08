2ff2f32b5c7c1547a389bc3bac461c1bca80e6bf
tawiah007 <davidtawiah027@gmail.com>
2022-12-08 15:42:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
     char Mystr[] = "\"Programming is like building a multilingual puzzle";
     puts(Mystr);
+
     return (0);
 }
