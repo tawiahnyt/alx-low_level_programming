cab074c191ba355a56e297ffe32a47f7c766f124
tawiah007 <davidtawiah027@gmail.com>
2022-12-08 15:45:11 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,9 @@
 
 int main(void)
 {
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    puts(Mystr);
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-    return (0);
+puts(Mystr);
+
+ return (0);
 }
