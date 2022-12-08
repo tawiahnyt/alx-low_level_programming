e556c8fda82595b57271ac978a6b8aa4c53152b3
tawiah007 <davidtawiah027@gmail.com>
2022-12-08 15:37:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main()
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return 0;
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
+    puts(Mystr);
+    return (0);
 }
