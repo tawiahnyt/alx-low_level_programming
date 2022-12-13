35d07bc539a2fc76117de7157ed1bf525a0aeb45
tawiah007 <davidtawiah027@gmail.com>
2022-12-13 09:12:12 -0800

    alphabet_10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			putchar(i);
+			ch++;
+		}
+
+		putchar('\n');
+		i++;
+	}
+}
