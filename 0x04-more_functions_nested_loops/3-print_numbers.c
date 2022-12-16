bad928eaf792dbdb30eab3241f5e115a35be6087
tawiah007 <davidtawiah027@gmail.com>
2022-12-16 05:16:00 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * @i: int to be looped through
+ * Return: prints 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+
+}
