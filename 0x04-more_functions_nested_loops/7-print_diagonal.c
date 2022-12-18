366d233eed03a4623aa47475b2c46a967044a30d
tawiah007 <davidtawiah027@gmail.com>
2022-12-18 07:41:46 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal lines on the termial
+ *
+ * @n - number of times the character \ should be printed
+ */
+
+void print_diagonal(int n)
+{   
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			if(j == i)
+				_putchar('\\');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+}
