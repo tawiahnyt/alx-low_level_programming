332c57d2db7d9bb29e120fc78379fcdeec26595e
tawiah007 <davidtawiah027@gmail.com>
2022-12-20 07:37:07 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a reverse of a string
+ *
+ * @s: the string to be printed
+ *
+ */
+
+void rev_string(char* s)
+{
+	int len, i;
+	char *start, *end, ch;
+
+	len = strlen(s);
+	start = s;
+	end = s + len - 1;
+
+	for (i = 0; i < (len - 1) / 2; i++) {
+
+		ch = *end;
+		*end = *start;
+		*start = ch;
+
+		start++;
+		end--;
+	}
+}
