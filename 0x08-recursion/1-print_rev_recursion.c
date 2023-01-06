169ac11ac7e96171082a7733a6443cdc347809ae
tawiah007 <davidtawiah027@gmail.com>
2023-01-06 13:37:22 -0800

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a sting in reverse
+ *
+ * @s: the string to be reversed
+ *
+ */
+
+void _print_rev_recursion(car *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		__putchar(*s);
+	}
+}
