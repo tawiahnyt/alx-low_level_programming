06b64118ddd43dd9260b127c4bfb440229f3ecc5
tawiah007 <davidtawiah027@gmail.com>
2023-01-06 13:49:55 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,11 +5,13 @@
  *
  * @s: length of the string to return
  *
+ * Return: length of s
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 		return(1 + _strlen_recursion(s + 1));
+
 	return(0);
 }
