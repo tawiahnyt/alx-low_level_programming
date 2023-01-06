7d360e428554722cae7ab1948231c287680c7c87
tawiah007 <davidtawiah027@gmail.com>
2023-01-06 02:59:23 -0800

    Recursion

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints strings followed by a newline
+ *
+ * @s: the string to return
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		;
+	}
+	prinft("%s\n", s)
+}
