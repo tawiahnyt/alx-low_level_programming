0b388f27843371d2a75fc9d96dcc96c2db5cbeeb
tawiah007 <davidtawiah027@gmail.com>
2023-01-09 06:58:24 -0800

    argc and argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/** main - prints all the arguements it receives
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * @Return: Return, always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+
+}
+
