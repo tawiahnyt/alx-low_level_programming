77d3969e1f06013344ee7269cad9d3cdded24d7c
tawiah007 <davidtawiah027@gmail.com>
2023-01-09 06:01:46 -0800

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * args - prints numbers of arguments passed into it
+ *
+ * @argc: argument counter
+ * @argv: argumetn vector
+ *
+ * Retrun: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
