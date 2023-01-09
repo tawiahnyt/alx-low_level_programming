78627f923cda2b72032c55fcea0045aa356be580
tawiah007 <davidtawiah027@gmail.com>
2023-01-09 05:28:42 -0800

    argc and argv

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints it's name, followed by a newline
+ *
+ * Return: Successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return (0);
+}
